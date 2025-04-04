250d37b5eae2f0ad5ec6f0a01ce9b383dd8beb30
starpets <135385839+starpets@users.noreply.github.com>
2025-04-04 14:12:08 +0500

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -1,1 +1,28 @@
+const puppeteer = require('puppeteer');
 
+let browser;
+let page;
+
+async function initializeBrowser() {
+    browser = await puppeteer.launch({ headless: false, slowMo: 5 });
+    page = await browser.newPage();
+    await page.goto('https://tbepanel.com/');
+
+    await page.type('#login', 'kampongsexi');
+    await page.type('#password', 'kampong12');
+
+    await Promise.all([
+        page.click('#submit_btn'),
+        await page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    ]);
+}
+
+function getPage() {
+    return page;
+}
+
+function getBrowser() {
+    return browser;
+}
+
+module.exports = { initializeBrowser, getPage, getBrowser };
